436efa5f4c601c7837b92d19bb128b55789b0cf7
Waleed <waleedthedev@gmail.com>
2022-01-09 16:39:24 +0500

    Clean up and setup according to style guides

---
+++ src/lib/footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+  <div class="text-center pt-10 pb-5">
+    <p>
+      Challenge by <a
+        href="https://www.frontendmentor.io?ref=challenge"
+        target="_blank"
+        class="underline">Frontend Mentor</a
+      >
+    </p>
+    <p>
+      Coded by <a
+        href="https://github.com/waleedmumtaz"
+        target="_blank"
+        class="font-bold underline">Waleed Mumtaz</a
+      >
+    </p>
+  </div>
+</footer>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,30 @@
 const config = {
-  content: ["./src/**/*.{html,js,svelte,ts}"],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cstm: {
+          primary: {
+            red: 'hsl(0, 100%, 74%)',
+            green: 'hsl(154, 59%, 51%)',
+          },
+          accent: {
+            blue: 'hsl(248, 32%, 49%)',
+          },
+          neutral: {
+            'dark-blue': 'hsl(249, 10%, 26%)',
+            'grayish-blue': 'hsl(246, 25%, 77%)',
+          },
+        },
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
 
   plugins: [],
-};
+}
 
-module.exports = config;
+module.exports = config
